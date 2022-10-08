3e310cee2bfe2342fab49a85c1cd57f7f7a571c2
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-10-08 22:54:26 +0200

    fix requirements issue

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-import os
-
 from setuptools import (
     find_packages,
     setup,
 )
 
 if __name__ == "__main__":
-    root_dir = os.path.dirname(__file__)
-    with open(os.path.join(root_dir, "requirements.txt")) as f:
-        requirements = [r.strip() for r in f]
-
-    setup(install_requires=requirements, packages=find_packages(where="src"), package_dir={"": "src"})
+    requirements = ["requests"]
+    setup(install_requires=requirements, packages=find_packages(where="src"), package_dir={"": "src"}, zip_safe=False)
