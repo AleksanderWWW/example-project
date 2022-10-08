cbe179fe227e079cd39a305baebc9aba241e0d0f
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-10-08 23:04:40 +0200

    resolve import error

--- src/tutorial/__init__.py
+++ src/tutorial/__init__.py
@@ -1,14 +1,18 @@
 __all__ = ["__version__", "Runner", "Tutorial"]
 
-from importlib.metadata import (
-    PackageNotFoundError,
-    version,
-)
+try:
+    from importlib.metadata import (
+        PackageNotFoundError,
+        version,
+    )
+
+    try:
+        __version__ = version("example-project")
+    except PackageNotFoundError:
+        # package is not installed
+        __version__ = None
+
+except ModuleNotFoundError:
+    __version__ = "0.0.1"
 
 from .tutorial_runner import *
-
-try:
-    __version__ = version("example-project")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = None
