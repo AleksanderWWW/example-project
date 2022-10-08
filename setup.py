40b9d891d8f44c90e93ea69c44cafa6687dee24c
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-10-08 22:57:38 +0200

    fix issue with python <3.8 and importlib

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import (
 )
 
 if __name__ == "__main__":
-    requirements = ["requests"]
+    requirements = ["requests", "importlib-metadata>=0.12"]
     setup(install_requires=requirements, packages=find_packages(where="src"), package_dir={"": "src"}, zip_safe=False)
